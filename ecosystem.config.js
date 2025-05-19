1fce5828f5156260179de3dabeaf20d375932ad9
duy59 <vuduy050903@gmail.com>
2025-05-19 19:32:31 +0700

    new update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,11 @@ module.exports = {
   apps: [
     {
       name: 'huyhoang-bookstore',
-      script: 'serve',
-      args: ['-s', '.', '-p', '9005'],
+      script: 'node',
+      args: ['server.js'],
       env: {
         NODE_ENV: 'production',
+        PORT: '9005'
       },
     },
   ],
